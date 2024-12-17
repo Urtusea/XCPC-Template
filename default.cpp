31e84e7e660305f7dd0d2238f01abb693a30c4fd
Urtusea <Urtusea@gmail.com>
2024-12-17 12:59:58 +0800

    update

---
+++ default.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define int  int32_t
+#define Int  int64_t
+#define uint uint32_t
+#define uInt uint64_t
+
+void init() {
+    
+}
+
+void code() {
+    
+}
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    for (int i = 1, n = (std::cin >> n, n); i <= n; i++)
+    {
+        init();
+        code();
+    }
+
+    return 0;
+}
