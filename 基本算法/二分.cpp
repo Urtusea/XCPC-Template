15ab464611fe00d77681078ced8434bd6b7061d1
Urtusea <Urtusea@gmail.com>
2024-12-17 13:13:42 +0800

    update

--- 基本算法/二分.cpp
+++ 基本算法/二分.cpp
@@ -1,24 +1,14 @@
 #include <bits/stdc++.h>
 
-template <int Mode, std::integral T>
+template <std::integral T>
 inline auto work(T l, T r, const auto &&func) {
-    if constexpr (Mode) {
-        if (func(r)) return r;
-    } else {
-        if (func(l)) return l;
-    }
-    
+    if (func(r)) return r;
     while (l + 1 < r) {
         const T mid = (l + r) >> 1;
-        if (func(mid)) {
-            if constexpr (Mode) l = mid;
-            else r = mid;
-        } else {
-            if constexpr (Mode) r = mid;
-            else l = mid;
-        }
+        if (func(mid))
+            l = mid;
+        else
+            r = mid;
     }
-
-    if constexpr (Mode) return l;
-    else return r;
+    return l;
 }
