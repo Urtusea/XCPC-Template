a366323133b4581ac348dda48a0063ade5d83826
Urtusea <Urtusea@gmail.com>
2024-12-17 13:32:45 +0800

    update

---
+++ 数据结构/线段树_单点修改.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+template <typename Info>
+class Segment_Tree_Bit {
+
+private:
+    const int offset;
+    std::vector<Info> node;
+    
+public:
+    Segment_Tree_Bit(int _n = 0) : offset(1 << (std::__lg(_n + 1) + 1)), node(2 << (std::__lg(_n + 1) + 1)) {}
+
+    void build(const std::vector<Info> &info) {
+        for (int i = 1; i < info.size(); i++)
+            node[i + offset] = info[i];
+        for (int i = offset - 1; i >= 1; i--)
+            node[i] = node[i << 1] + node[i << 1 | 1];
+    }
+
+    void update(int u, Info x) {
+        for (int i = u + offset; i; i >>= 1)
+            node[i] += x;
+    }
+
+    Info query(int l, int r, Info res = Info()) {
+        for (int i = l + offset - 1, j = r + offset + 1; i ^ j ^ 1; i >>= 1, j >>= 1) {
+            if ((i & 1) == 0) res += node[i ^ 1];
+            if ((j & 1) == 1) res += node[j ^ 1];
+        }
+        return res;
+    }
+};
+
+struct info {
+
+    info &operator += (info other) {
+    }
+
+    info operator + (info other) {
+    }
+};
