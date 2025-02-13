16da52ef51ec9448f0b41e36316eb69da77fb477
Urtusea <Urtusea@gmail.com>
2025-02-13 17:15:31 +0800

    update

--- 数据结构/Segment_Tree/Segment_Tree_Dynamic_Lazy.cpp
+++ 数据结构/Segment_Tree/Segment_Tree_Dynamic_Lazy.cpp
@@ -5,19 +5,30 @@
 #define uint uint32_t
 #define uInt uint64_t
 
-template <typename Info, typename Lazy, uint Size>
+enum int_size {
+    int_4m   = 1 << 20,
+    int_8m   = 1 << 21,
+    int_16m  = 1 << 22,
+    int_32m  = 1 << 23,
+    int_64m  = 1 << 24,
+    int_128m = 1 << 25,
+    int_256m = 1 << 26,
+    int_512m = 1 << 27
+};
+
+template <typename Info, typename Lazy, int Size>
 struct Segment_Tree_Dynamic {
     struct Node {
         Info data;
         Lazy post;
-        uint lson;
-        uint rson;
+        int lson;
+        int rson;
 
         Node()
         : data(Info()), post(Lazy()), lson(0), rson(0) {}
     };
-    uint last;
-    Node node[Size / (sizeof(Node) / sizeof(uint))];
+    int last;
+    Node node[Size / (sizeof(Node) / sizeof(int))];
 
     Segment_Tree_Dynamic()
     : last(0) {}
@@ -27,11 +38,11 @@ struct Segment_Tree_Dynamic {
         last = 0;
     }
 
-    void push_up(uint p) {
+    void push_up(int p) {
         node[p].data = node[node[p].lson].data + node[node[p].rson].data;
     }
 
-    void push_down(uint p, int l, int r) {
+    void push_down(int p, int l, int r) {
         if (!node[p].lson) node[p].lson = ++last;
         if (!node[p].rson) node[p].rson = ++last;
         const auto edit = [&](int u, int node_size) -> void {
@@ -44,7 +55,7 @@ struct Segment_Tree_Dynamic {
         node[p].post.clear();
     }
 
-    void update(uint &p, int l, int r, int L, int R, Lazy tag) {
+    void update(int &p, int l, int r, int L, int R, Lazy tag) {
         if (!p) p = ++last;
         if (R < l || r < L)
             return;
@@ -57,7 +68,7 @@ struct Segment_Tree_Dynamic {
         push_up(p);
     }
 
-    Info query(uint p, int l, int r, int L, int R) {
+    Info query(int p, int l, int r, int L, int R) {
         if (!p || R < l || r < L)
             return Info();
         if (L <= l && r <= R)
@@ -66,11 +77,4 @@ struct Segment_Tree_Dynamic {
         push_down(p, l, r);
         return query(node[p].lson, l, m, L, R) + query(node[p].rson, m + 1, r, L, R);
     }
-};
-
-enum uint_size {
-    uint_64m  = 1 << 24,
-    uint_128m = 1 << 25,
-    uint_256m = 1 << 26,
-    uint_512m = 1 << 27
 };
