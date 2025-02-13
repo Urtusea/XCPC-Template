e7714e264e5011d989aac67df31287f077062d07
Urtusea <Urtusea@gmail.com>
2025-02-13 17:16:00 +0800

    update

--- 数据结构/Segment_Tree/Segment_Tree_Dynamic.cpp
+++ 数据结构/Segment_Tree/Segment_Tree_Dynamic.cpp
@@ -5,18 +5,29 @@
 #define uint uint32_t
 #define uInt uint64_t
 
-template <typename Info, uint Size>
+enum int_size {
+    int_4m   = 1 << 20,
+    int_8m   = 1 << 21,
+    int_16m  = 1 << 22,
+    int_32m  = 1 << 23,
+    int_64m  = 1 << 24,
+    int_128m = 1 << 25,
+    int_256m = 1 << 26,
+    int_512m = 1 << 27
+};
+
+template <typename Info, int Size>
 struct Segment_Tree_Dynamic {
     struct Node {
         Info data;
-        uint lson;
-        uint rson;
+        int lson;
+        int rson;
 
         Node()
         : data(Info()), lson(0), rson(0) {}
     };
-    uint last;
-    Node node[Size / (sizeof(Node) / sizeof(uint))];
+    int last;
+    Node node[Size / (sizeof(Node) / sizeof(int))];
 
     Segment_Tree_Dynamic()
     : last(0) {}
@@ -26,11 +37,11 @@ struct Segment_Tree_Dynamic {
         last = 0;
     }
 
-    void push_up(uint p) {
+    void push_up(int p) {
         node[p].data = node[node[p].lson].data + node[node[p].rson].data;
     }
 
-    void update(uint &p, int l, int r, int u, Info x) {
+    void update(int &p, int l, int r, int u, Info x) {
         if (!p) p = ++last;
         if (u < l || r < u)
             return;
@@ -42,7 +53,7 @@ struct Segment_Tree_Dynamic {
         push_up(p);
     }
 
-    Info query(uint p, int l, int r, int L, int R) {
+    Info query(int p, int l, int r, int L, int R) {
         if (!p || R < l || r < L)
             return Info();
         if (L <= l && r <= R)
@@ -50,11 +61,4 @@ struct Segment_Tree_Dynamic {
         int m = (l + r) >> 1;
         return query(node[p].lson, l, m, L, R) + query(node[p].rson, m + 1, r, L, R);
     }
-};
-
-enum uint_size {
-    uint_64m  = 1 << 24,
-    uint_128m = 1 << 25,
-    uint_256m = 1 << 26,
-    uint_512m = 1 << 27
 };
