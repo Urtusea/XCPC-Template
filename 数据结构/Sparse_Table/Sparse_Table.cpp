805e81c69cc352c150cc86a129271deddff983fd
Urtusea <Urtusea@gmail.com>
2025-02-18 17:48:33 +0800

    update

--- 数据结构/Sparse_Table/Sparse_Table.cpp
+++ 数据结构/Sparse_Table/Sparse_Table.cpp
@@ -5,12 +5,9 @@
 #define uint uint32_t
 #define uInt uint64_t
 
-template <typename Info, typename Comp, int N> struct Sparse_Table {
+template <typename Info, typename Comp, Comp Func(Info, Info), int N> struct Sparse_Table {
     int n;
-    Comp g;
     Info f[N * (std::__lg(N) + 1)];
-
-    Sparse_Table(auto &&_g) : g(_g) {}
 
     inline constexpr int pos(int i, int j) const noexcept {
         return i * n + j;
@@ -25,11 +22,11 @@ template <typename Info, typename Comp, int N> struct Sparse_Table {
     void build() {
         for (int j = 1; j <= std::__lg(n); j++)
             for (int i = 1; i + (1 << j) - 1 <= n; i++)
-                f[pos(j, i)] = g(f[pos(j - 1, i)], f[pos(j - 1, i + (1 << (j - 1)))]);
+                f[pos(j, i)] = Func(f[pos(j - 1, i)], f[pos(j - 1, i + (1 << (j - 1)))]);
     }
 
     Info query(int l, int r) {
         int k = std::__lg(r - l + 1);
-        return g(f[pos(k, l)], f[pos(k, r - (1 << k) + 1)]);
+        return Func(f[pos(k, l)], f[pos(k, r - (1 << k) + 1)]);
     }
 };
