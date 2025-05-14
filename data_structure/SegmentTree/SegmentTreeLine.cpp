0ba76c5a7c62e4454ce6ed4bfb13985d08d3d32a
Urtusea <Urtusea@gmail.com>
2025-05-14 20:16:05 +0800

    update

--- data_structure/SegmentTree/SegmentTreeLine.cpp
+++ data_structure/SegmentTree/SegmentTreeLine.cpp
@@ -8,8 +8,8 @@
 template <typename T> struct Line {
   using type = T;
   T k, b;
-  Line() {}
-  constexpr T operator () (int x) const {
+  Line() : k(0), b(0) {}
+  constexpr T operator () (Int x) const {
     return k * x + b;
   }
 };
@@ -18,14 +18,15 @@ template <typename Info, auto Comp, int N> struct SegmentTree {
   using Type = typename Info::type;
   Info f[(4 << std::__lg(N)) + 1];
 
-  void push_down(int p, int l, int r, const Info& line) {
+  void push_down(int p, int l, int r, Info line) {
     int m = (l + r) / 2 + 1;
     if (Comp(f[p], line, m)) std::swap(f[p], line);
+    if (l == r) return;
     if (Comp(f[p], line, l)) push_down(p << 1, l, m - 1, line);
     if (Comp(f[p], line, r)) push_down(p << 1 | 1, m, r, line);
   }
 
-  void update(int p, int l, int r, int L, int R, const Info& line) {
+  void update(int p, int l, int r, int L, int R, const Info line) {
     if (L <= l && r <= R) return push_down(p, l, r, line);
     int m = (l + r) / 2 + 1;
     if (L <  m) update(p << 1, l, m - 1, L, R, line);
@@ -37,7 +38,7 @@ template <typename Info, auto Comp, int N> struct SegmentTree {
     int m = (l + r) / 2 + 1;
     Type res = f[p](x);
     if (x <  m) res = std::max(res, query(p << 1, l, m - 1, x));
-    if (x >= m) res = std::max(res, query(p << 1 | 1, r, m, x));
+    if (x >= m) res = std::max(res, query(p << 1 | 1, m, r, x));
     return res;
   }
 };
